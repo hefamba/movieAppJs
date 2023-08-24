0cdf97f80fd84852619699a1392b196a2086aafd
hefamba <81347556+hefamba@users.noreply.github.com>
2023-08-23 21:02:51 -0500

    added url

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-const API_URL = 'https://api.themoviedb.org/3/movie/157336?api_key=307b40941d0fad058e7110084ed9cd95=1';
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc%27&api_key=307b40941d0fad058e7110084ed9cd95';
+
