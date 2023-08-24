9973796d909ab830275b1fea35f0419d9595cc34
hefamba <81347556+hefamba@users.noreply.github.com>
2023-08-23 22:09:52 -0500

    done with search time for DOM

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc%27&api_key=307b40941d0fad058e7110084ed9cd95';
+const api_key = 'api_key=307b40941d0fad058e7110084ed9cd95'
+
+const API_URL = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc%27&${api_key}`;
 
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280/';
 
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&api_key=307b40941d0fad058e7110084ed9cd95&query="';
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&${api_key}&query="`;
+
+
 
 
 const form = document.getElementById('form');
